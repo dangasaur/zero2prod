2577e48e6832aa97c742978c2eef3eabfde6608c
dangasaur <dangasaur@gmail.com>
2023-02-26 19:34:13 -0600

    working through chapter 3

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::net::TcpListener;
+
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run("0.0.0.0:8000")?.await
+    let listener = TcpListener::bind("0.0.0.0:8000")
+        .expect("Failed to bind port 8000");
+    run(listener)?.await
 }
